a7030bd5f7ebd08a73e244d90fba53d7debc0ec4
Digvijay <sawaledigvijay70@gmail.com>
2026-07-14 23:04:29 +0530

    add table and cards partial and update views

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   root "pages#home"
   get "/about", to: "pages#about"
+  get "/upcoming", to: "tasks#upcoming"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
